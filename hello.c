c09a55a5bb478596d70130450aa2f88d2ebcd8f4
hanscai <hongshengcai88@gmail.com>
2016-01-17 22:15:13 +0800

    first c file

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: hello_world.c
+    > Author: Hans
+    > Mail: hongshengcai88@gmail.com 
+    > Created Time: 2016年01月17日 星期日 21时57分02秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main (void)
+{
+	printf("hello world!\n");
+	return 0;
+}
