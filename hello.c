7b529402e6f96986aee0bf52fe49478facec3893
hanscai <hongshengcai88@gmail.com>
2016-03-10 23:09:00 +0800

    change hello

--- hello.c
+++ hello.c
@@ -1,14 +1,20 @@
 /*************************************************************************
     > File Name: hello.c
     > Author: Hans
-    > Mail: hongshengcai88@gmail.com 
+    > Mail: hongshengcai88@gmail.com
     > Created Time: 2016年01月26日 星期二 23时31分52秒
  ************************************************************************/
 
 #include<stdio.h>
 int main (void)
 {
-	printf("Hello Hans!\n");
-	printf("Good night!\n");
-	return 0;
+    int i;
+
+    printf("start!\n");
+    for(i=0; i<10; i++)
+    {
+        printf("Good night! %d !\n",i);
+    }
+
+    return 0;
 }
